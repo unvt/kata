14297cc35f3e8d5249ca041f27b7a6660867d848
Takayuki Miyauchi <miya0001@users.noreply.github.com>
2022-03-22 21:48:57 +0900

    added tests

--- test/filter.spec.ts
+++ test/filter.spec.ts
@@ -10,6 +10,7 @@ describe('test the filter command.', () => {
     assert.deepEqual('this is a point', geojsons[0].features[0].properties.name)
     assert.deepEqual('this is a polygon', geojsons[0].features[1].properties.name)
     assert.deepEqual('hello world', geojsons[1].features[0].properties.title)
+    assert.deepEqual('This is a geojson on the gist.github.com', geojsons[2].features[0].properties.title)
 
     assert.deepEqual(8, geojsons[0].tippecanoe.minzoom)
     assert.deepEqual(14, geojsons[0].tippecanoe.maxzoom)
